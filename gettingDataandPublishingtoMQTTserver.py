927c26d352e26da8a44aa1c14d6930f354855ae8
arshlakhani <arshlakhani146@gmail.com>
2024-04-08 22:16:29 +0530

    defining a function

--- gettingDataandPublishingtoMQTTserver.py
+++ gettingDataandPublishingtoMQTTserver.py
@@ -8,35 +8,34 @@ def on_connect(client, userdata, flags, rc):
 clientID = "arsh"
 broker = "mqtt-dashboard.com"
 port = 8884
-pub = mqtt.Client(clientID)
 
-pub.on_connect = on_connect
-pub.connect('broker.mqttdashboard.com', 1883)
-pub.publish("testtopicnew/1","hi",qos= 1)
-connection = mysql.connect('foodmenu.db')
-cursor = connection.cursor()
-orderID = int(input("What food are we making today"))
-ordertype = input('d for drink and f for food')
-if ordertype == 'f':
-    cursor.execute('SELECT * FROM menu WHERE id = {}'.format(orderID))
-    row_headers = [x[0] for x in cursor.description]
-    rows = cursor.fetchall()
-    print(rows)
-    json_data = []
-    for row in rows:
-        json_data.append(dict(zip(row_headers, row)))
-    json_Data = json.dumps(json_data)
-    print(json_Data)
-if ordertype == 'd':
-    cursor.execute('SELECT * FROM drinksnew WHERE id = {}'.format(orderID))
-    row_headers = [x[0] for x in cursor.description]
-    rows = cursor.fetchall()
-    print(rows)
-    json_data = []
-    for row in rows:
-        json_data.append(dict(zip(row_headers, row)))
-    json_Data = json.dumps(json_data)
-    print(json_Data)
-pub.publish("testtopicnew/1",f"{json_Data}",qos= 1)
-connection.commit()
-connection.close()
+def publishdata(clientID,  broker, port, topic, foodID):
+    pub = mqtt.Client(clientID)
+    pub.on_connect = on_connect
+    pub.connect('broker.mqttdashboard.com', 1883)
+    connection = mysql.connect('foodmenu.db')
+    cursor = connection.cursor()
+   
+    if foodID < 200:
+        cursor.execute('SELECT * FROM menu WHERE id = {}'.format(foodID))
+        row_headers = [x[0] for x in cursor.description]
+        rows = cursor.fetchall()
+        print(rows)
+        json_data = []
+        for row in rows:
+            json_data.append(dict(zip(row_headers, row)))
+        json_Data = json.dumps(json_data)
+        print(json_Data)
+    else:
+        cursor.execute('SELECT * FROM drinksnew WHERE id = {}'.format(foodID))
+        row_headers = [x[0] for x in cursor.description]
+        rows = cursor.fetchall()
+        print(rows)
+        json_data = []
+        for row in rows:
+            json_data.append(dict(zip(row_headers, row)))
+        json_Data = json.dumps(json_data)
+        print(json_Data)
+    pub.publish(topic,f"{json_Data}",qos= 1)
+    connection.commit()
+    connection.close()
